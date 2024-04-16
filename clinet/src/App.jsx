3355e6463e9fa1b53bffd759e9b7ccd7b3919849
gaurav9689 <124897570+gaurav9689@users.noreply.github.com>
2024-04-16 14:52:34 +0530

    create pages and routes

--- clinet/src/App.jsx
+++ clinet/src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Dashbord from './pages/Dashbord'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashbord" element={<Dashbord />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
